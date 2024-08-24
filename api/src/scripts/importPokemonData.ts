44f5006f7e564f85614e1b1a65b5a16795273354
Josue Bacca <josuebacca.sistemas@gmail.com>
2024-08-23 21:22:10 -0300

    ADD: clean comments

--- api/src/scripts/importPokemonData.ts
+++ api/src/scripts/importPokemonData.ts
@@ -1,24 +1,19 @@
-// importPokemonData.ts
-
 import { DataSource } from 'typeorm';
 import { Pokemon } from '../pokemon/entities/pokemon.entity';
-import {pokemon as pokemonData} from '../../data/pokemon.json'; // Importa los datos desde el JSON
+import {pokemon as pokemonData} from '../../data/pokemon.json';
 
-// Configuración de la conexión usando DataSource
 const dataSource = new DataSource({
   type: 'sqlite',
   database: 'pokemon-battle.db',
   entities: [Pokemon],
-  synchronize: false, // Sincronización solo en migraciones
+  synchronize: false, 
 });
 
 async function importPokemonData() {
   try {
-    // Inicializar la conexión
     await dataSource.initialize();
     const pokemonRepository = dataSource.getRepository(Pokemon);
 
-    // Mapear los datos del JSON ignorando el campo 'id'
     const pokemons = pokemonData.map((data) => {
       const pokemon = new Pokemon();
       pokemon.name = data.name;
@@ -31,13 +26,11 @@ async function importPokemonData() {
       return pokemon;
     });
 
-    // Guardar los datos en la base de datos
     await pokemonRepository.save(pokemons);
     console.log('Datos importados correctamente');
   } catch (error) {
     console.error('Error al importar los datos:', error);
   } finally {
-    // Finalizar la conexión
     await dataSource.destroy();
   }
 }
